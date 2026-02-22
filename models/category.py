2c4bd87fc314279b71e949912867ae97462ded58
mfbangher-art <mfbangher@gmail.com>
2026-02-21 21:51:36 -0300

    Update category.py

--- models/category.py
+++ models/category.py
@@ -1,1 +1,23 @@
+# models/category.py
+import sqlite3
+from db import get_connection
+
+class Category:
+    @staticmethod
+    def create(name):
+        with get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                "INSERT INTO categories (name) VALUES (?)",
+                (name,)
+            )
+            conn.commit()
+            return cursor.lastrowid
+
+    @staticmethod
+    def get_all():
+        with get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT id, name FROM categories")
+            return cursor.fetchall()
 
